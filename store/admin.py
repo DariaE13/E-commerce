d7a4574bd79d7c848d00e935c833d62658089bfd
DariaE13 <daria-10@internet.ru>
2025-08-30 09:09:16 +0200

    Сделана отдельная страница для каждого  товара

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Category
-from .models import Product
+from .models import Category,Product
 
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Product)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','slug','price','stock','is_available','created_at']
+    list_filter = ['is_available','category']
+    list_editable = ['price','stock','is_available']
+    prepopulated_fields = {'slug': ('name',)}
--- store/apps.py
+++ store/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
-
+# настройка приложения store
 class StoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
+    # Название приложения
     name = 'store'
